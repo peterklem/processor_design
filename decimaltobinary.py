64e158451c143e29850947df00998a835c901add
trmyint97 <trmyint97@gmail.com>
2020-07-06 14:21:55 -0400

    Adding decimal to binary function

---
+++ decimaltobinary.py
@@ -0,0 +1,16 @@
+def decimaltobinary(x, n=0):
+    """
+    Get the binary representation of x.
+
+    Parameters
+    ----------
+    x : int
+    n : int
+        Minimum number of digits. If x needs less digits in binary, the rest
+        is filled with zeros.
+
+    Returns
+    -------
+    str
+    """
+    return format(x, 'b').zfill(n)
