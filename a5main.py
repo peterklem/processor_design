328f54e82e39954e718300b34b07311790a7ce40
peterklem <klembczykp@wit.edu>
2020-07-27 14:19:02 -0400

    small logic change

--- a5main.py
+++ a5main.py
@@ -19,6 +19,7 @@ for i in range(32):
 
 reg_dict['ZR'] = 0
 
+
 print(reg_dict)
 
 DMEM = {}
@@ -26,9 +27,9 @@ DMEM = {}
 for i in range(10):
     choice = input('Press 1 to change DMEM values or 0 to stop: ')
     choice = int(choice)
-    if choice == 0:
+    if choice != 1: # Stop
         break
-    elif choice == 1:
+    elif choice == 1: # Change MEM value
         DMEM_address = input('Enter from 1 to 10 the DMEM address you want to change: ')
         value = input('Enter value inside the DMEM: ')
         value = int(value)
